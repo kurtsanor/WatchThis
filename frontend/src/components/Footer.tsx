da19217279b0ea00da56b2b7341e26d42a934690
Kurt <sanorkurtkelly@gmail.com>
2025-08-31 22:13:19 +0800

    refactor: footer credits

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,12 +8,24 @@ function Footer() {
         alignItems: "center",
         marginBottom: "1rem",
         marginTop: "4rem",
+        borderTop: "solid 1px #404040",
       }}
     >
-      <p style={{ textAlign: "center" }}>Movie data and images provided by</p>
+      <p style={{ textAlign: "center", fontSize: "0.85rem" }}>
+        Movie data and images provided by
+      </p>
       <a href="https://www.themoviedb.org/">
         <img src="/TMBD logo.svg" alt="TMBD logo" height={30}></img>
       </a>
+      <p
+        style={{
+          textAlign: "center",
+          fontSize: "0.85rem",
+          fontStyle: "italic",
+        }}
+      >
+        This product uses the TMDb API but is not endorsed or certified by TMDb.
+      </p>
     </footer>
   );
 }
