86351507415cb0d897b9b3b5103c5c7d7bec3ece
Kurt Sanor <sanorkurtkelly@gmail.com>
2026-02-24 23:17:45 +0800

    update jwt expiration

--- backend/src/utils/jwtUtil.js
+++ backend/src/utils/jwtUtil.js
@@ -4,7 +4,7 @@ const generateToken = (user) => {
   return jwt.sign(
     { id: user._id.toString(), email: user.email },
     process.env.JWT_SECRET_KEY,
-    { expiresIn: "1s" },
+    { expiresIn: "7d" },
   );
 };
 
