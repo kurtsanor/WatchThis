c412a92a596f427a17df9c627c71bb0b5bcc8352
Kurt Sanor <sanorkurtkelly@gmail.com>
2026-03-01 23:51:30 +0800

    revert

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,31 +1,19 @@
 const mongoose = require("mongoose");
 
-let cached = global.mongoose;
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
 async function getConnection() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(process.env.MONGODB_URI, {
-        autoIndex: true,
-        dbName: "watchthis_db",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then((mongoose) => {
-        console.log("MongoDB connected");
-        return mongoose;
-      })
-      .catch((err) => {
-        console.error("MongoDB connection failed:", err);
-        throw err;
-      });
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await mongoose.connect(process.env.MONGODB_URI, {
+      autoIndex: true,
+      dbName: "watchthis_db",
+    });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+    process.exit(1); // stop server if DB fails
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
 
 module.exports = getConnection;
